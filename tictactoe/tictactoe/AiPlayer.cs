9da071c092ccb45d7948d089a91fe4c63d77a283
KELHETTER Hugo <hugo.kelhetter@lgcf.fr>
2024-12-19 09:38:53 +0100

    wip

---
+++ tictactoe/tictactoe/AiPlayer.cs
@@ -0,0 +1,22 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace TicTacToe;
+
+public class AiPlayer : IPlayer
+{
+    public AiPlayer(char icon)
+    {
+        this.icon = icon;
+    }
+
+    public char icon { get; init; }
+
+    public Result<PlayerMoves> GetNextMove()
+    {
+        var random = new Random();
+        var targetRow = random.Next(1, 4);
+        var targetColumn = random.Next(1, 4);
+
+        return Result.Success(new PlayerMoves(targetRow, targetColumn));
+    }
+}
---
+++ tictactoe/tictactoe/Game.cs
@@ -0,0 +1,67 @@
+﻿namespace TicTacToe;
+
+internal class Game
+{
+    public static readonly char PlayerOneIcon = 'O';
+    public static readonly char PlayerTwoIcon = 'X';
+
+    private readonly Board board;
+    private readonly IPlayer player1;
+    private readonly IPlayer player2;
+
+    public Game(IPlayer player1, IPlayer player2, Board board)
+    {
+        this.board = board;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public IPlayer currentPlayer { get; private set; }
+
+    public void Init()
+    {
+        board.Init();
+        currentPlayer = player1;
+    }
+
+    public void Play()
+    {
+        board.DisplayGameBoardAndHeader();
+
+        while (true)
+        {
+            var playerMoves = currentPlayer.GetNextMove();
+            if (playerMoves.IsFailure)
+            {
+                Console.WriteLine(playerMoves.Error);
+                continue;
+            }
+
+            var movePlayedSuccessfully = board.PlayMoveOnBoard(playerMoves.Value, currentPlayer.icon);
+            if (movePlayedSuccessfully is false)
+            {
+                Console.WriteLine("Invalid move");
+                continue;
+            }
+
+            board.DisplayGameBoardAndHeader();
+
+            var gameResult = board.IsGameOver(currentPlayer);
+            if (gameResult.HasValue)
+            {
+                Console.WriteLine(gameResult.Value);
+                break;
+            }
+
+            SwitchPlayer();
+        }
+    }
+
+    private void SwitchPlayer()
+    {
+        if (currentPlayer == player1)
+            currentPlayer = player2;
+        else
+            currentPlayer = player1;
+    }
+}
