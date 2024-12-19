df811d14f43a9c0ba6fea600ab46595503875bba
KELHETTER Hugo <hugo.kelhetter@lgcf.fr>
2024-12-19 11:46:04 +0100

    fix

---
+++ tictactoe/tictactoeTest/PlayerMoveTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using TicTacToe;
+using TicTacToe.Players;
+
+namespace tictactoeTest;
+
+public class PlayerMoveTest
+{
+    [Fact]
+    public void TestRandomPlayer()
+    {
+        var player = new RandomPlayer(PlayerConstants.PlayerOneIcon);
+
+        var move = player.GetNextMove();
+
+        move.IsSuccess.Should().BeTrue();
+        move.Value.Row.Should().BeInRange(1, 3);
+        move.Value.Column.Should().BeInRange(1, 3);
+    }
+}
